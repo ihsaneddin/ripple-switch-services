91efa3936f75f34bc69733f2ba1aead2139fab92
ihsaneddin <ihsaneddin@gmail.com>
2017-12-07 10:07:00 +0700

    fix typo

--- app/helpers/image_view_helper.rb
+++ app/helpers/image_view_helper.rb
@@ -3,7 +3,7 @@ module ImageViewHelper
   #
   # sugar method to display avatar
   #
-  def user_avatar_image(url="/assets/adminLTE/img/user2-160x160", options={})
+  def user_avatar_image(url="/assets/adminLTE/img/user2-160x160.png", options={})
     image_tag url, { class: 'avatar-image', alt: 'User Image' }.merge!(options)
   end
 
