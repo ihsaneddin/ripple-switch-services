99f3e51364c23bda77daa653651ae988e50275b0
ihsaneddin <ihsaneddin@gmail.com>
2017-12-22 17:16:18 +0700

    fix typo

--- app/modules/ripples/workers/wallet_broadcaster_worker.rb
+++ app/modules/ripples/workers/wallet_broadcaster_worker.rb
@@ -9,7 +9,7 @@ module Ripples
 
       def perform(wallet_id)
         wallet = Ripples::Models::Wallet.find(wallet_id)
-        ActionCable.server.broadcast "wallet-#{wallet.address}", message: wallet.to_h
+        ActionCable.server.broadcast "wallet-#{wallet.address}", message: wallet.to_json
         ActionCable.server.broadcast "balance-#{wallet.account.id}", message: { total_balance: wallet.account.total_balance, total_balance_xrp: wallet.account.total_balance_xrp }.to_json
       end
 
