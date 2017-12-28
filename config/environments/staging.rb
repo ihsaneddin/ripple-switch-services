b324851b54f96a1799e82bddf0aabffc86f7589b
ihsaneddin <ihsaneddin@gmail.com>
2017-12-28 16:13:20 +0700

    silence hashie warning

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -102,8 +102,9 @@ Rails.application.configure do
      enable_starttls_auto: true
   }
 
-  config.action_cable.allowed_request_origins = ['http://139.59.107.166']
+  #config.action_cable.mount_path = nil
   config.action_cable.url = "ws://139.59.107.166/cable"
+  config.action_cable.allowed_request_origins = [ 'http://139.59.107.166' ]
 
 end
 #Rails.application.config.action_cable.disable_request_forgery_protection = true
