0c0e74034518ec5b85d026bbf02960a8e6d9cdcc
ihsaneddin <ihsaneddin@gmail.com>
2017-12-28 17:48:44 +0700

    debug connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,11 +4,13 @@ module ApplicationCable
     identified_by :current_account
 
     def connect
+      logger.add_tags "TESTCABLE", "ASUP"
       self.current_account = find_verified_account
     end
 
     protected
       def find_verified_account
+        return Users::Models::Account.first
         if current_account = env['warden'].user
           Rails.logger.info "Verified #{current_account}"
           current_account
