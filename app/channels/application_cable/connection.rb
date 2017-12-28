72dfcb69167a8a89f717be2d85e542e54db68aef
ihsaneddin <ihsaneddin@gmail.com>
2017-12-28 18:00:29 +0700

    change staging log level

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -4,18 +4,14 @@ module ApplicationCable
     identified_by :current_account
 
     def connect
-      logger.add_tags "TESTCABLE", "ASUP"
       self.current_account = find_verified_account
     end
 
     protected
       def find_verified_account
-        return Users::Models::Account.first
-        if current_account = env['warden'].user
-          Rails.logger.info "Verified #{current_account}"
+        if current_account= env['warden'].user
           current_account
         else
-          Rails.logger.info "Unverified"
           reject_unauthorized_connection
         end
       end
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :info#:debug
+  config.log_level = :debug #:info#
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
